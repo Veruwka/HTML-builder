a88272243d46e3b8d4be7996487017f6fc95f7d5
VeraTiutiunnik <revkaa@gmail.com>
2022-05-22 22:53:12 +0300

    feat: add code for second task of HTML builder

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('node:path')
+const readline = require('readline');
+const { stdin, stdout } = process;
+
+let outputFile = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+stdout.write('Привет! Напиши что-нибудь:');
+
+let rl = readline.createInterface({ 
+    input: stdin,
+    output: outputFile,    
+});
+
+rl.on('SIGINT', () => {
+    stdout.write('До новых встречь!!!');
+});
+
+rl.on('line', line => {
+    if (line === 'exit') {
+        rl.close();
+        stdout.write('До новых встречь!!!');
+    } else {
+        outputFile.write(line + '\n');
+    }
+});

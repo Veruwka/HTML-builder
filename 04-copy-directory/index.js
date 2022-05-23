fe66a16fa71fbdd31d55c1f49219ae8a78e8e749
VeraTiutiunnik <revkaa@gmail.com>
2022-05-23 14:02:07 +0300

    feat: create a programm to copy a folder with all the files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('node:path');
+const fsPromises = require('fs').promises;
 
+const inputDir = path.join(__dirname, 'files');
+const outputDir = path.join(__dirname, 'files-copy');
+
+fs.mkdir(outputDir, { recursive: true }, (err) => {
+    if (err) return console.log(err);
+});
+
+async function copyFiles(dir) {
+    let directory = await fs.promises.opendir(dir);
+    for await (let file of directory) {
+        let name = file.name;
+        fsPromises.copyFile(path.join(dir, name), path.join(outputDir, `copy-${name}`))
+        .then (function () {
+            console.log('File was copied)');
+        })
+        .catch (function (error) {
+            console.log(error);
+        })
+    }
+}
+copyFiles(inputDir);

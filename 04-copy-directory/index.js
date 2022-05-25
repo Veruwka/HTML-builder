8e9d6b1aa91061563b7f6878d5179aab721b0cd4
VeraTiutiunnik <revkaa@gmail.com>
2022-05-25 14:02:51 +0300

    refactor: add delete file function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -11,6 +11,16 @@ fs.mkdir(outputDir, { recursive: true }, (err) => {
 
 async function copyDir(dir) {
     let directory = await fs.promises.opendir(dir);
+    let outDir = await fs.promises.opendir(outputDir);
+
+    for await (let fileCopy of outDir) {
+        let a = fileCopy.name;
+        fs.unlink(path.join(outputDir, a), (err) => {
+            if (err) console.log(err);
+            else console.log('file was deleted');
+        });
+    }
+
     for await (let file of directory) {
         let name = file.name;
         fsPromises.copyFile(path.join(dir, name), path.join(outputDir, `copy-${name}`))
